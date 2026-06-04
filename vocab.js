6f44e84ce0c028c39b6c9171e941da1d893e8af2
dacdaik97 <caogiakhoi@gmail.com>
2026-06-04 14:00:26 +0700

    Delete vocab.js

--- vocab.js
+++
@@ -1,9 +0,0 @@
-const words = [
-
-{hsk:"HSK1",lesson:"Bài 1",cn:"你好",py:"nǐ hǎo",vn:"Xin chào"},
-{hsk:"HSK1",lesson:"Bài 1",cn:"谢谢",py:"xièxie",vn:"Cảm ơn"},
-{hsk:"HSK1",lesson:"Bài 2",cn:"再见",py:"zàijiàn",vn:"Tạm biệt"},
-{hsk:"HSK2",lesson:"Bài 1",cn:"老师",py:"lǎoshī",vn:"Giáo viên"},
-{hsk:"HSK2",lesson:"Bài 2",cn:"学生",py:"xuésheng",vn:"Học sinh"}
-
-];
